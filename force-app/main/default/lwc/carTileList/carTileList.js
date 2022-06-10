2df32731107212409671175f638bdd6f49f219c2
zachsienk <zachsienk@gmail.com>
2022-06-10 11:03:57 -0500

    First commit

--- force-app/main/default/lwc/carTileList/carTileList.js
+++ force-app/main/default/lwc/carTileList/carTileList.js
@@ -1,3 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getCars from '@salesforce/apex/CarController.getCars'
 
-export default class CarTileList extends LightningElement {}
+//Lightning Message Service and message channel
+import{subscribe, MessageContext} from 'lightning/messageService'
+import CARS_FILTERED_MESSAGE from  '@salesforce/messageChannel/carsFiltered__c'
+
+export default class CarTileList extends LightningElement {
+    cars
+    error
+    filters = {};
+    carFilterSubscription
+    @wire(getCars, {filters: '$filters'})
+    carsHandleer({data, error}){
+        if(data){
+            console.log(data)
+            this.cars = data
+        }
+        if(error){
+            this.error = error
+            console.error(error)
+            
+        }
+    }
+
+       //** Load context for LMS */
+       @wire(MessageContext)
+       messageContext
+
+       connectedCallback(){
+           this.subscribeHandler()
+       }
+
+       subscribeHandler(){
+           this.carFilterSubscription = subscribe(this.messageContext, CARS_FILTERED_MESSAGE, (message)=>this.handleFilterChanges(message))
+       }
+       handleFilterChanges(message){
+           console.log(message.filters)
+       }
+}
